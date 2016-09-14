a84ad9b2da62a47491dbc1433fd721005c8af370
Soraya Ibrahim <soraya_ibrahim@live.nl>
2016-09-14 20:07:05 +0200

    add express depenencies

---
+++ server.js
@@ -0,0 +1,4 @@
+//const and let de waarde kan niet worden gewijzigd het kan maar 1 keer gedeclareerd worden
+// var hoisting javascript door var functions te gebruiken zorg je ervoor dat je de functie altijd kan gebruiken omdat het als variable gezien wordt
+
+var express = require('express');
