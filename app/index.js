236a75c8abe58995212a32bc2df39c56c6111f0e
Soraya Ibrahim <soraya_ibrahim@live.nl>
2016-09-14 22:25:25 +0200

    handlebars

--- app/index.js
+++ app/index.js
@@ -3,13 +3,27 @@ var app = express();
 var motivations = require('motivations');
 // hiermee geef je aan waar
 var pickOne = require('pick-one');
+var exphbs = require('express-handlebars')
+
+app.use(express.static('./app/public'));
+
+app.engine('handlebars', exphbs ({
+	defaultLayout: 'main',
+	layoutsDir:'./app/views/layouts'
+}));
+
+app.set('views', './app/views')
+app.set('view engine', 'handlebars')
+
+//je verwijst met motivation naar de file motivation in views
 app.get('/', function(request, response){
-	response.send(pickOne(motivations));
+	response.render('motivation', {motivation: pickOne(motivations)});
+	// response.send(pickOne(motivations));
 });
 
-app.get('/:motivationsID', function(request, response){
-	var id = request.params.motivationsID;
-	response.send(id);
-});
+// app.get('/:motivationsID', function(request, response){
+// 	var id = request.params.motivationsID;
+// 	response.send(id);
+// });
 
 module.exports =  app;
